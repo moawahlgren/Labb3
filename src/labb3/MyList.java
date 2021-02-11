ff32039a0957abcf3f2744ff0732f78708242291
moawahlgren <77838783+moawahlgren@users.noreply.github.com>
2021-02-11 09:55:06 +0100

    hhef
    skrev ba från dokumentet du gjorde

--- src/labb3/MyList.java
+++ src/labb3/MyList.java
@@ -9,6 +9,43 @@ package labb3;
  *
  * @author moawahlgren
  */
-public interface MyList {
+public class MyList<E> implements java.lang.Iterable<E> {
+   
+    private Node<E> head;
+    private Node<E> tail; 
+    private int size; 
+    
+    MyList() {
+        head = null; 
+        tail = null; 
+        size = 0; 
+    }
+    
+    private static class Node<E> {
+        E element; 
+        Node<E> nextElement; 
+        
+        public Node(E element) {
+            this.element = element; 
+        }
+        public Node(E element, Node<E> nextElement) {
+            this.element = element; 
+            this.nextElement = nextElement; 
+        }
+        
+    }
+    
+    public int size() {
+        return size; 
+    }
+    
+    public void add(E element) {
+        tail = new Node(element, tail);
+        size ++; 
+    }
+    
+    
+ 
+    
     
 }
